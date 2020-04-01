76c750f368b619b4454ddc7bd1c1ba25043757af
izawayuusuke <yuu0502@gmail.com>
2020-04-01 14:40:25 +0000

    Finish bookers2-debug

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,48 +1,65 @@
 class BooksController < ApplicationController
+	before_action :authenticate_user!
+	before_action :set_book, only: [:edit, :update]
 
-  def show
-  	@book = Book.find(params[:id])
+	def show
+		@new_book = Book.new
+		@book = Book.find(params[:id])
+		@user = User.find(@book.user_id)
   end
 
-  def index
-  	@books = Book.all #一覧表示するためにBookモデルの情報を全てくださいのall
+	def index
+		@book = Book.new
+		@books = Book.all
   end
 
   def create
-  	@book = Book.new(book_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
-  	if @book.save #入力されたデータをdbに保存する。
-  		redirect_to @book, notice: "successfully created book!"#保存された場合の移動先を指定。
-  	else
-  		@books = Book.all
-  		render 'index'
-  	end
+		@book = Book.new(book_params)
+		@book.user_id = current_user.id
+		if @book.save
+			redirect_to @book, notice: "successfully created book!"#保存された場合の移動先を指定。
+		else
+			@user = current_user
+			@books = Book.all
+			render 'index'
+		end
   end
 
   def edit
-  	@book = Book.find(params[:id])
+		@book = Book.find(params[:id])
   end
 
 
 
   def update
-  	@book = Book.find(params[:id])
-  	if @book.update(book_params)
-  		redirect_to @book, notice: "successfully updated book!"
-  	else #if文でエラー発生時と正常時のリンク先を枝分かれにしている。
-  		render "edit"
-  	end
+		@book = Book.find(params[:id])
+		if @book.update(book_params)
+			redirect_to @book, notice: "successfully updated book!"
+		else #if文でエラー発生時と正常時のリンク先を枝分かれにしている。
+			render "edit"
+		end
   end
 
-  def delete
-  	@book = Book.find(params[:id])
-  	@book.destoy
-  	redirect_to books_path, notice: "successfully delete book!"
+  def destroy
+		@book = Book.find(params[:id])
+		@book.destoy
+		redirect_to books_path, notice: "successfully delete book!"
   end
 
   private
 
   def book_params
-  	params.require(:book).permit(:title)
-  end
+		params.require(:book).permit(:title, :body)
+	end
 
+	def user_params
+		params.require(:user).permit(:name, :introduction, :profile_image)
+	end
+
+	def set_book
+		@book = Book.find(params[:id])
+		unless @book.user == current_user
+			redirect_to books_path
+		end
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable,:validatable
-
-  belongs_to :books
-  attachment :profile_image, destroy: false
-
-  #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
-  validates :name, length: {maximum: 20, minimum: 2}
+          :recoverable, :rememberable, :trackable,:validatable
+  has_many :books, dependent: :destroy
+  attachment :profile_image
+  validates :name, presence: true, length: { maximum: 20, minimum: 2 }
+  validates :introduction, length: { maximum: 50 }
 end
